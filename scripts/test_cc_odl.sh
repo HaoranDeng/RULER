0c25799e9d3ed9913c3049a506c65f1e53979fa1
HaoranDeng <deng.hr@163.com>
2025-09-03 16:12:12 +0000

    uopdate

--- scripts/test_cc_odl.sh
+++ scripts/test_cc_odl.sh
@@ -1,19 +1,19 @@
-export MY_MODEL_NAME="cc-vanilla-250"
-export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-vanilla/checkpoint-250"
+export MY_MODEL_NAME="cc-odl-250"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-odl/checkpoint-250"
 bash run.sh my_model synthetic
 mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
 
-export MY_MODEL_NAME="cc-vanilla-500"
-export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-vanilla/checkpoint-500"
+export MY_MODEL_NAME="cc-odl-500"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-odl/checkpoint-500"
 bash run.sh my_model synthetic
 mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
 
-export MY_MODEL_NAME="cc-vanilla-750"
-export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-vanilla/checkpoint-750"
+export MY_MODEL_NAME="cc-odl-750"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-odl/checkpoint-750"
 bash run.sh my_model synthetic
 mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
 
-export MY_MODEL_NAME="cc-vanilla-1000"
-export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-vanilla/checkpoint-1000"
+export MY_MODEL_NAME="cc-odl-1000"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_cc-odl/checkpoint-1000"
 bash run.sh my_model synthetic
 mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
