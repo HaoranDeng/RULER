6172953426b530012c7a70da98c6380597b5db5c
HaoranDeng <deng.hr@163.com>
2025-09-03 12:13:38 +0000

    update

---
+++ scripts/test_code_odl.sh
@@ -0,0 +1,19 @@
+export MY_MODEL_NAME="code-odl-250"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_code-odl/checkpoint-250"
+bash run.sh my_model synthetic
+mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
+
+export MY_MODEL_NAME="code-odl-500"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_code-odl/checkpoint-500"
+bash run.sh my_model synthetic
+mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
+
+export MY_MODEL_NAME="code-odl-750"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_code-odl/checkpoint-750"
+bash run.sh my_model synthetic
+mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
+
+export MY_MODEL_NAME="code-odl-1000"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_code-odl/checkpoint-1000"
+bash run.sh my_model synthetic
+mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
