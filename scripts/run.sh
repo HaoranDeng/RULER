ce50858e936d7b967971a3622ccd738a54675c58
HaoranDeng <deng.hr@163.com>
2025-09-03 12:14:29 +0000

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,11 +23,11 @@ fi
 
 
 # Root Directories
-GPUS="1" # GPU size for tensor_parallel.
+GPUS="8" # GPU size for tensor_parallel.
 ROOT_DIR="benchmark_root" # the path that stores generated task samples and model predictions.
 MODEL_DIR=".." # the path that contains individual model folders from HUggingface.
 ENGINE_DIR="." # the path that contains individual engine folders from TensorRT-LLM.
-BATCH_SIZE=1  # increase to improve GPU utilization
+BATCH_SIZE=8  # increase to improve GPU utilization
 
 
 # Model and Tokenizer
