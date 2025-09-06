fee6413af48b62cf88dbeb3ecc91c1b22c48ef31
HaoranDeng <deng.hr@163.com>
2025-09-06 14:59:21 +0000

    update

--- scripts/config_models.sh
+++ scripts/config_models.sh
@@ -33,6 +33,11 @@ MODEL_SELECT() {
             MODEL_TEMPLATE_TYPE="base"
             MODEL_FRAMEWORK="vllm"
             ;;
+        llama3.1-8b-chat)
+            MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/Llama-3.1-8B-Instruct"
+            MODEL_TEMPLATE_TYPE="base"
+            MODEL_FRAMEWORK="vllm"
+            ;;
         llama3.1-8b)
             MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/Llama-3.1-8B"
             MODEL_TEMPLATE_TYPE="base"
