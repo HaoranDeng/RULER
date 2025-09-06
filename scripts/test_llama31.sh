bbc8b4c8a070918e9a3981e23f8221ad7519748b
HaoranDeng <deng.hr@163.com>
2025-09-06 05:55:54 +0000

    update

---
+++ scripts/test_llama31.sh
@@ -0,0 +1,4 @@
+export MY_MODEL_NAME="Llama-3.1-8B"
+export MY_MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/Llama-3.1-8B"
+bash run.sh my_model synthetic
+mv benchmark_root/* /mnt/blob-pretraining-hptraining/haoran_result/RULER/.
